702845a873570cbcd1c39eb3eb15ddb920212c6b
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-02-11 17:27:17 +0300

    changed js static files

--- application/__init__.py
+++ application/__init__.py
@@ -68,6 +68,7 @@ def create_app():
 
         # Serve static files
         app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+            '/static/img': os.path.join(app.config.get('PROJECT_PATH'), '/static/img'),
             '/static': os.path.join(app.config.get('PROJECT_PATH'), 'output/static'),
             '/pkg': os.path.join(app.config.get('PROJECT_PATH'), 'output/pkg'),
             '/pages': os.path.join(app.config.get('PROJECT_PATH'), 'output/pages')
